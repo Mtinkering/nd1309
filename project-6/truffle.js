d50d73312546f4046288108326bd7c12a9fca173
Steven Nguyen <steven.nguyenhung@gmail.com>
2021-08-24 00:56:39 +0800

    Prepare the project

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,24 @@
+const provider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const secret = JSON.parse(fs.readFileSync(".secret.json").toString().trim());
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: "*", // Match any network id
+    },
+
+    rinkeby: {
+      provider: () =>
+        new provider(
+          secret.mnemonic,
+          `https://rinkeby.infura.io/v3/${secret.infuraKey}`,
+          0,
+          3
+        ),
+      network_id: 4,
+    },
+  },
 };
---
+++ write-up.md
@@ -0,0 +1,8 @@
+# Transaction ID
+# The contract address
+# If libraries were used, include why these libraries were adopted.
+# If IPFS is used, include how IPFS is used in this project.
+# Program version numbers (This information will help your reviewer troubleshoot your project if any issues arise):
+- node version number
+- Truffle version number
+- web3 version number
